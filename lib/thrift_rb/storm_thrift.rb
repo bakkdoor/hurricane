9bc1a5d18ff2c32d05e0a4f2f36f1a8e4b9a7fd8
Christopher Bertels <bakkdoor@flasht.de>
2011-10-17 18:26:23 -0700

    added thrift generated ruby files

---
+++ lib/thrift_rb/storm_thrift.rb
@@ -0,0 +1,9 @@
+require "rubygems"
+require "thrift"
+
+$: << File.expand_path(File.dirname(__FILE__))
+
+require "storm_types"
+require "storm_constants"
+require "nimbus"
+require "distributed_r_p_c"
